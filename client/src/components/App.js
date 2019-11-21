a4ec092a5add535ee7a07771894092bc8c51c09e
marinaikeno <marinaikeno@gmail.com>
2019-11-20 22:24:22 -0800

    added git repo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div>
       <h1>Sudoku Solver</h1>
+      <a target="_blank" href="https://github.com/marinaikeno/sudoku_solver"><h2><i className="fa fa-github" /></h2></a>
       <p>
         You're ready to give up on your sudoku?
       </p>
