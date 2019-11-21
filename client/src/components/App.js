65d5450f6862f4c20039051883f0c0643cc5fcf9
marinaikeno <marinaikeno@gmail.com>
2019-11-20 22:40:46 -0800

    updated link icon

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <h1>Sudoku Solver</h1>
-      <a target="_blank" href="https://github.com/marinaikeno/sudoku_solver"><h2><i className="fa fa-github" /></h2></a>
+      <h2><a target="_blank" href="https://github.com/marinaikeno/sudoku_solver"><i className="fa fa-github" /></a></h2>
       <p>
         You're ready to give up on your sudoku?
       </p>
